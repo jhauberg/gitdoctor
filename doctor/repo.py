baee18c879009a713c121aca256abe2f8af2e86f
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2018-11-28 21:08:37 +0100

    Add initial scrubdown implementation

--- doctor/repo.py
+++ doctor/repo.py
@@ -42,3 +42,29 @@ def has_integrity() -> (bool, List[str]):
     issues = result.stderr.splitlines()
 
     return False, issues
+
+
+def absolute_path() -> str:
+    """ Return the absolute path of the repository. """
+
+    result = subprocess.run([
+        'git', 'rev-parse', '--show-toplevel'],
+        check=True,  # print stacktrace on non-zero exit status
+        stdout=subprocess.PIPE,  # capture stdout
+        stderr=subprocess.DEVNULL)  # ignore stderr
+
+    path = str(result.stdout)
+
+    return path
+
+
+def size_in_bytes() -> int:
+    """ Return the size (in bytes) of the entire repository. """
+
+    path = absolute_path()
+
+    files = (os.path.join(dirpath, filename) for dirpath, dirnames, filenames in os.walk(path) for filename in filenames)
+    filesizes = [os.path.getsize(filepath) for filepath in files if not os.path.islink(filepath)]
+    size = sum(filesizes)
+
+    return size
--- doctor/scrub.py
+++ doctor/scrub.py
@@ -6,7 +6,19 @@ Provides cleaning functions for the current repository.
 
 import subprocess
 
+import doctor.repo as repo
+
 
 def trim():
-    pass
+    size_before = repo.size_in_bytes()
 
+    subprocess.run(['git', 'gc', '--auto', '--aggressive', '--no-prune', '--quiet'], check=True,
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(['git', 'prune'], check=True, stdout=subprocess.DEVNULL,
+                   stderr=subprocess.DEVNULL)
+
+    size_after = repo.size_in_bytes()
+
+    size_difference = max(size_before, size_after) - min(size_before, size_after)
+
+    print(f'saved {size_difference} bytes')
